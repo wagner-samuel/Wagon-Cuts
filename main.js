e8a4e5461004089f872db97795632445eac71a49
Sam Bowcut <sbowcut@gmail.com>
2021-06-17 15:52:15 -0600

    fixing

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
+const url = 'https://showy-dynamic-icebreaker.glitch.me/movies';
+
+$(".submitBTN").click(makePost);
+function getInputVals (){
+    let newMovie = {
+        title: $(`#inputTitle`).val(),
+        rating: $(`#inputRating`).val(),
+        poster: $(`#inputPoster`).val(),
+        year: $(`#inputYear`).val(),
+        genre: $(`#inputGenre`).val(),
+        director: $(`#inputDirector`).val(),
+        plot: $(`#inputPlot`).val(),
+        actors: $(`#inputActors`).val()
+    };
+    return newMovie;
+}
+
+function makePost(){
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(getInputVals()),
+    };
+    console.log(getInputVals())
+    fetch(url, options)
+        .then( response => console.log(response))
+        .catch( error => console.error(error))
+        .then(function(html){
+            location.reload();
+        });
+}
 var delay = 1500;
 var timeoutId = setTimeout(function () {
-    $(".loadingMessage").text(`We have a great selection of movies!`)
+    $(".loadingMessage").text("Look at all these Movies")
     // $(".loadingMessage").append(${data})
 }, delay);
 
-$.get("https://showy-dynamic-icebreaker.glitch.me/movies", {
+$.get(url, {
 }).done(function(data) {
     data.forEach(function (data) {
         $('.movieList').append(movieCards(data))
@@ -36,47 +69,8 @@ function movieCards(data) {
 }
 
 
-const url = 'https://showy-dynamic-icebreaker.glitch.me/movies';
-
 // // ---- notes
-$(".subitBTN").click(makePost);
 
-
-function getInputVals (){
-    let newMovie = {
-        title: $(`#inputTitle`).val(),
-        rating: $(`#inputRating`).val(),
-        poster: $(`#inputPoster`).val(),
-        year: $(`#inputYear`).val(),
-        genre: $(`#inputGenre`).val(),
-        director: $(`#inputDirector`).val(),
-        plot: $(`#inputPlot`).val(),
-        actors: $(`#inputActors`).val()
-    };
-    return newMovie;
-    // console.log(newMovie)
-    // grab all vals of input fields
-    // create a new object variable
-    // take vals and put them into the object
-    // call another function : makePostReq(obj)
-}
-
-function makePost(){
-    const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(getInputVals()),
-};
-    console.log(getInputVals())
-fetch(url, options)
-    .then( response => console.log(response) ) /* review was created successfully */
-    .catch( error => console.error(error) ); /* handle errors */
-    // post request with the object as the data
-    // fetch request
-    // .. then ...
-}
 
 
 
