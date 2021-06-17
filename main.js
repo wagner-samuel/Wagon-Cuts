8bb9275fbe661cdbd9d435ec3cc05e573c2d391a
Sam Bowcut <sbowcut@gmail.com>
2021-06-17 13:40:00 -0600

    post-data

--- main.js
+++ main.js
@@ -1,55 +1,55 @@
-// var delay = 1500;
-// var timeoutId = setTimeout(function () {
-//     $(".loadingMessage").text("These are our movies")
-//     // $(".loadingMessage").append(${data})
-// }, delay);
-//
-// const newMovie = {
-//     "title": "Up",
-//     "rating": "5",
-//     "poster": "https://m.media-amazon.com/images/M/MV5BYWMwMzQxZjQtODM1YS00YmFiLTk1YjQtNzNiYWY1MDE4NTdiXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg",
-//     "year": "2010",
-//     "genre": "Action, Comedy, Drama",
-//     "director": "Sam Bowcut",
-//     "plot": "Guy goes up",
-//     "actors": "Wagner Charles"
-// };
-// const url = 'https://showy-dynamic-icebreaker.glitch.me/movies';
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(newMovie),
-// };
-// fetch(url, options)
-//     .then( response => console.log(response) ) /* review was created successfully */
-//     .catch( error => console.error(error) ); /* handle errors */
-// fetch(url)
-//     .then( response => console.log(response) )
+var delay = 1500;
+var timeoutId = setTimeout(function () {
+    $(".loadingMessage").text("These are our movies")
+    // $(".loadingMessage").append(${data})
+}, delay);
+
+
+const url = 'https://showy-dynamic-icebreaker.glitch.me/movies';
 
 function showValues() {
     var str = $( "form" ).serialize();
     console.log(str)
     $( "#results" ).text( str );
 }
-$(".subitBTN").click( function (){
 
-} );
 
 // // ---- notes
-// $(".subitBTN").click( getInputVals());
-//
-// getInputVals (){
-//     // grab all vals of input fields
-//     // create a new object variable
-//     // take vals and put them into the object
-//     // call another function : makePostReq(obj)
-// }
-//
-// makePost(){
-//     // post request with the object as the data
-//     // fetch request
-//     // .. then ...
-// }
+$(".subitBTN").click(makePost);
+
+function getInputVals (){
+    let newMovie = {
+        title: $(`#inputTitle`).val(),
+        rating: $(`#inputRating`).val(),
+        poster: $(`#inputPoster`).val(),
+        year: $(`#inputYear`).val(),
+        genre: $(`#inputGenre`).val(),
+        director: $(`#inputDirector`).val(),
+        plot: $(`#inputPlot`).val(),
+        actors: $(`#inputActors`).val()
+    };
+    return newMovie;
+    // console.log(newMovie)
+    // grab all vals of input fields
+    // create a new object variable
+    // take vals and put them into the object
+    // call another function : makePostReq(obj)
+}
+
+function makePost(){
+    const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(getInputVals()),
+};
+    console.log(getInputVals())
+fetch(url, options)
+    .then( response => console.log(response) ) /* review was created successfully */
+    .catch( error => console.error(error) ); /* handle errors */
+    // post request with the object as the data
+    // fetch request
+    // .. then ...
+}
 
